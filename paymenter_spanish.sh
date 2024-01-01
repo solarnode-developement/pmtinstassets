1945636b11f699698e534561793a617f0db7c085
SantiagolxxGG <149891004+SantiagolxxGG@users.noreply.github.com>
2024-01-01 19:18:30 -0300

    Update paymenter_spanish.sh

--- paymenter_spanish.sh
+++ paymenter_spanish.sh
@@ -1,18 +1,15 @@
-#__________           _________              __  .__                      .__                   ________  ________ 
-#\______   \___.__.  /   _____/____    _____/  |_|__|____     ____   ____ |  | ___  ______  ___/  _____/ /  _____/ 
-# |    |  _<   |  |  \_____  \\__  \  /    \   __\  \__  \   / ___\ /  _ \|  | \  \/  /\  \/  /   \  ___/   \  ___ 
-# |    |   \\___  |  /        \/ __ \|   |  \  | |  |/ __ \_/ /_/  >  <_> )  |__>    <  >    <\    \_\  \    \_\  \
-# |______  // ____| /_______  (____  /___|  /__| |__(____  /\___  / \____/|____/__/\_ \/__/\_ \\______  /\______  /
-#        \/ \/              \/     \/     \/             \//_____/                   \/      \/       \/        \/ 
 #!/bin/bash
 
-# Preguntas interactivas al usuario
-read -p "Ingresa el dominio que deseas utilizar en Paymenter (por ejemplo, paymenter.org, billing.mycoolhost.com): " domain
-read -p "¿Deseas configurar SSL automáticamente? (y/n): " configure_ssl
-read -p "¿Quieres utilizar un certificado SSL con Certbot? (Si no tines certificado.) (y/n): " use_certbot
+# Ruta al archivo .env de Paymenter
+ruta_env="/var/www/paymenter/.env"
+
+# Preguntas interactivas para el usuario
+read -p "Ingresa el dominio que deseas utilizar para Paymenter (por ejemplo, paymenter.org, billing.mycoolhost.com): " dominio
+read -p "¿Quieres configurar SSL automáticamente? (s/n): " configurar_ssl
+read -p "¿Quieres utilizar un certificado SSL con Certbot? (Si no tienes un certificado.) (s/n): " usar_certbot
 
 # Validación del dominio
-if [ -z "$domain" ]; then
+if [ -z "$dominio" ]; then
     echo "El dominio no puede estar vacío."
     exit 1
 fi
@@ -44,26 +41,26 @@ if ! command -v nginx &> /dev/null; then
     apt -y install nginx
 fi
 
-# Verificar la existencia del directorio /var/www/paymenter
+# Verifica la existencia del directorio /var/www/paymenter
 if [ -d "/var/www/paymenter" ]; then
-    read -p "El directorio /var/www/paymenter ya existe. ¿Quieres eliminarlo y continuar? (y/n): " delete_existing
-    if [ "$delete_existing" = "y" ]; then
+    read -p "El directorio /var/www/paymenter ya existe. ¿Quieres eliminarlo y continuar? (s/n): " eliminar_existente
+    if [ "$eliminar_existente" = "s" ]; then
         rm -rf /var/www/paymenter
         echo "Directorio existente eliminado."
     else
-        echo "Operación cancelada. No se ha realizado ningún cambio."
+        echo "Operación cancelada. No se han realizado cambios."
         exit 1
     fi
 fi
 
-# Configuración SSL
-if [ "$configure_ssl" = "y" ]; then
+# Configuración de SSL
+if [ "$configurar_ssl" = "s" ]; then
     # Instalación de Certbot
     apt -y install certbot python3-certbot-nginx
 
     # Solicitar certificado SSL con Certbot
-    if [ "$use_certbot" = "y" ]; then
-        certbot --nginx -d $domain
+    if [ "$usar_certbot" = "s" ]; then
+        certbot --nginx -d $dominio
     fi
 fi
 
@@ -77,9 +74,9 @@ tar -xzvf paymenter.tar.gz
 chmod -R 755 storage/* bootstrap/cache/
 
 # Crear usuario y base de datos en MySQL
-read -p "¿Quieres un host externo? (y/n): " external_host
-if [ "$external_host" = "y" ]; then
-    read -p "Introduce el host externo que quieres usar. Si no pones nada, se pondrá automáticamente 127.0.0.1: " ext_host
+read -p "¿Quieres un host externo? (s/n): " host_externo
+if [ "$host_externo" = "s" ]; then
+    read -p "Ingresa el host externo que deseas usar. Si lo dejas en blanco, se utilizará 127.0.0.1: " ext_host
     ext_host=${ext_host:-127.0.0.1}
     read -p "Ingresa el nombre de la base de datos (presiona Enter para usar 'paymenter'): " db_name
     db_name=${db_name:-paymenter}
@@ -88,31 +85,31 @@ if [ "$external_host" = "y" ]; then
     read -p "Ingresa la contraseña de la base de datos (presiona Enter para generar una contraseña aleatoria): " db_password
     db_password=${db_password:-$(openssl rand -hex 16)}
 else
-# Creación de usuario y base de datos en MySQL
-mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
-mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';"
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+    # Crear usuario y base de datos en MySQL
+    mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
+    mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';"
+    mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
 fi
 
 # Configurar archivo .env
 cp .env.example .env
-# dependecias de composer
+# Dependencias de Composer
 composer install --no-dev --optimize-autoloader
 
-# Edición del archivo .env
+# Editar archivo .env
 sed -i "/^DB_HOST=/s/.*/DB_HOST=$ext_host/" .env
 sed -i "/^DB_DATABASE=/s/.*/DB_DATABASE=$db_name/" .env
 sed -i "/^DB_USERNAME=/s/.*/DB_USERNAME=$db_user/" .env
 sed -i "/^DB_PASSWORD=/s/.*/DB_PASSWORD=$db_password/" .env
 
-# Generar clave de la aplicación
+# Generar clave de aplicación
 php artisan key:generate --force
 
 # Ejecutar migraciones y seeders
 php artisan migrate --force --seed
 
-# Crear enlace simbólico para el almacenamiento
+# Crear enlace simbólico para storage
 php artisan storage:link
 
 # Crear contraseña
@@ -123,22 +120,22 @@ nginx_conf="/etc/nginx/sites-available/paymenter.conf"
 echo "server {" > $nginx_conf
 echo "    listen 80;" >> $nginx_conf
 echo "    listen [::]:80;" >> $nginx_conf
-echo "    server_name $domain;" >> $nginx_conf
+echo "    server_name $dominio;" >> $nginx_conf
 
-if [ "$configure_ssl" = "y" ]; then
+if [ "$configurar_ssl" = "s" ]; then
     echo "    return 301 https://\$host\$request_uri;" >> $nginx_conf
     echo "}" >> $nginx_conf
     echo "" >> $nginx_conf
     echo "server {" >> $nginx_conf
     echo "    listen 443 ssl http2;" >> $nginx_conf
     echo "    listen [::]:443 ssl http2;" >> $nginx_conf
-    echo "    server_name $domain;" >> $nginx_conf
+    echo "    server_name $dominio;" >> $nginx_conf
     echo "    root /var/www/paymenter/public;" >> $nginx_conf
     echo "" >> $nginx_conf
     echo "    index index.php;" >> $nginx_conf
     echo "" >> $nginx_conf
-    echo "    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;" >> $nginx_conf
-    echo "    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;" >> $nginx_conf
+    echo "    ssl_certificate /etc/letsencrypt/live/$dominio/fullchain.pem;" >> $nginx_conf
+    echo "    ssl_certificate_key /etc/letsencrypt/live/$dominio/privkey.pem;" >> $nginx_conf
     echo "" >> $nginx_conf
     echo "    location / {" >> $nginx_conf
     echo "        try_files \$uri \$uri/ /index.php?\$query_string;" >> $nginx_conf
@@ -169,13 +166,13 @@ ln -s $nginx_conf /etc/nginx/sites-enabled/
 chown -R www-data:www-data /var/www/paymenter/*
 systemctl restart nginx
 
-# Agregar cronjob para Laravel Scheduler
+# Añadir cronjob para Laravel Scheduler
 (crontab -l ; echo "* * * * * php /var/www/paymenter/artisan schedule:run >> /dev/null 2>&1") | crontab -
 
-# Configurar el servicio de Paymenter
+# Configurar servicio Paymenter
 paymenter_service="/etc/systemd/system/paymenter.service"
 echo "[Unit]" > $paymenter_service
-echo "Description=Paymenter Queue Worker" >> $paymenter_service
+echo "Description=Cola de trabajo de Paymenter" >> $paymenter_service
 echo "" >> $paymenter_service
 echo "[Service]" >> $paymenter_service
 echo "User=www-data" >> $paymenter_service
@@ -193,9 +190,9 @@ echo "WantedBy=multi-user.target" >> $paymenter_service
 systemctl enable --now paymenter
 
 # Mensaje de finalización
-if [ "$configure_ssl" = "y" ]; then
-    echo "Tu instalación de Paymenter está disponible en: https://$domain"
+if [ "$configurar_ssl" = "s" ]; then
+    echo "Tu instalación de Paymenter está disponible en: https://$dominio"
 else
-    echo "Tu instalación de Paymenter está disponible en: http://$domain"
+    echo "Tu instalación de Paymenter está disponible en: http://$dominio"
 fi
 echo "¡Gracias por usar este script!"
